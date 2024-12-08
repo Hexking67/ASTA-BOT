5a6499b226681c12e013360e889aae677bb6f216
Hexking67 <160383006+Hexking67@users.noreply.github.com>
2024-12-08 19:31:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "SBQiHbQD#2jt-X3JuSk9Lvu6I-QBdMJX27dHiLqsdPBXMvj_XNrk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/whar66.jpg",
-SUDO_NB: process.env.SUDO_NB || "254114127157",
+SUDO_NB: process.env.SUDO_NB || "254797303045",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
